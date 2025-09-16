f4b914a22bc8b09b859dd969380a7da776b7a64d
radiolagames <radiolagames@gmail.com>
2025-09-16 04:07:14 +0000

    Changes before Firebase Studio auto-run

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:myapp/providers/alarm_provider.dart';
-import 'package:myapp/pages/home_screen.dart';
 import 'package:myapp/services/notification_controller.dart';
 import 'package:myapp/controllers/settings_controller.dart';
 import 'package:myapp/services/settings_service.dart';
